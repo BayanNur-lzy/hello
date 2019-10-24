5d4c1d8e8cb3be89bd123967b5693907d148f137
zhuzhao <du_2018@qq.com>
2019-10-24 13:12:17 +0800

    1

---
+++ hllgy/src/main/java/com/llby/hllgy/HllgyApplication.java
@@ -0,0 +1,13 @@
+package com.llby.hllgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HllgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HllgyApplication.class, args);
+	}
+
+}
---
+++ hllgy/src/test/java/com/llby/hllgy/HllgyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.llby.hllgy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HllgyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
